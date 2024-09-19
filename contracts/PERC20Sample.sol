4c6b804e061424815cd2a97271f90e9252434fc4
mayamedinathoma <mayamedinathomascb59w0rysa@hotmail.com>
2024-09-19 15:44:30 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("288730b2dba6518262cd9f7e22664e3f1083853247203cb401234f7222493b4a", "288730b2dba6518262cd9f7e22664e3f1083853247203cb401234f7222493b4a") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
